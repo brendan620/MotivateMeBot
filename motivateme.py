71e050a45dd8895bb7e310f62f52db3d6bd821f3
Brendan Lilly <brendan620@gmail.com>
2016-11-16 17:39:12 +0000

    Set up skeleton

---
+++ motivateme.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from secret import *
+import tweepy
+
+auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
+auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+
+
