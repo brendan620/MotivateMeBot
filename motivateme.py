f4aed5023519928f3840b020a00c6c4ff13fab27
Brendan Lilly <brendan620@gmail.com>
2016-11-16 18:44:09 +0000

    Added quotes and set up reply

--- motivateme.py
+++ motivateme.py
@@ -2,10 +2,26 @@
 
 from secret import *
 import tweepy
+import time
+import random
+
 
 auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
 auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 api = tweepy.API(auth)
 
+lastUser=""
 
-
+while True:
+	
+	mentionList = api.mentions_timeline(1)
+	for mention in mentionList:
+		if(lastUser != mention.user.screen_name):
+			lineList = open("quotes.txt").read().splitlines()
+			quote = random.choice(lineList)
+			tweet_body = "@" + mention.user.screen_name + " " + quote.decode('utf-8')
+			#print tweet_body
+			api.update_status(tweet_body ,mention.id)
+			lastUser=mention.user.screen_name
+			
+	time.sleep(30)
