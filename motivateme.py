d6d125fc8d282a02043a83e528e685bf6fec158d
Brendan Lilly <brendan620@gmail.com>
2016-11-16 23:32:26 +0000

    Fixed loop

--- motivateme.py
+++ motivateme.py
@@ -10,18 +10,28 @@ auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
 auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-lastUser=""
+idFile=open("lastID.txt")
+lastTweet=idFile.readline()
 
 while True:
 	
-	mentionList = api.mentions_timeline(1)
+	mentionList = api.mentions_timeline(count=1)
 	for mention in mentionList:
-		if(lastUser != mention.user.screen_name):
+                
+		if(lastTweet != str(mention.id)):
 			lineList = open("quotes.txt").read().splitlines()
 			quote = random.choice(lineList)
 			tweet_body = "@" + mention.user.screen_name + " " + quote.decode('utf-8')
-			#print tweet_body
-			api.update_status(tweet_body ,mention.id)
-			lastUser=mention.user.screen_name
+			while(len(tweet_body) > 140):
+                                quote = random.choice(lineList)
+                                tweet_body = "@" + mention.user.screen_name + " " + quote.decode('utf-8')        
+                        api.update_status(tweet_body ,mention.id)
+			lastTweet=mention.id
+                        idFile=open("lastID.txt",'w')
+                        idFile.write(str(lastTweet))
+                        
+                        
 			
 	time.sleep(30)
+
+idFile.close()
